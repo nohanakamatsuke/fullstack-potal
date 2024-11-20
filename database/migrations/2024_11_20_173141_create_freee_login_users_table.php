ba1ecb94e46444176d1c93bb85d98200aa52e6e6
Y.inoue <kisin1010@gmail.com>
2024-11-20 18:18:41 +0900

    pull時のリモート環境とのコンフリクト解消

---
+++ database/migrations/2024_11_20_173141_create_freee_login_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('freee_login_users', function (Blueprint $table) {
+            $table->id('freee_login_id');
+            $table->integer('employee_id');
+            $table->string('freee_login_mail');
+            $table->string('freee_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('freee_login');
+    }
+};
