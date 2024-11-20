79a9b91a772dabf03fbf36d7ce0fc65a0839fe1c
やなぎさわ <y.y.baske.74c@gmail.com>
2024-11-20 17:46:34 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2024_11_20_173842_create_notification_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->id('notification_id');
+            $table->integer('employee_id');
+            $table->string('notification_type',50);
+            $table->text('message');
+            $table->integer('complete_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notification');
+    }
+};
