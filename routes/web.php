ac0981c89cc4cbf8da9249705a8c8173763eebfe
Y.inoue <kisin1010@gmail.com>
2024-11-21 15:34:17 +0900

    seederファイルユーザー情報一件作成成功

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get( '/', [ SessionController::class, 'session' ] );
-
+Route::get('/', [SessionController::class, 'session']);
+//ログイン用ルーティング
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
