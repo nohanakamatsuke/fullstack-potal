923a5bc94d3ba65c84c0640957c693a3abe658b8
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-22 17:32:09 +0900

    ホームボタン作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,21 @@
         <title>Fullstack-Portal</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
     </head>
-<body>
     <body class="font-mono antialiased">
-        <div class="flex flex-col items-center justify-center">
-            <div class="w-full h-16">
-              {{-- ID:{{$id}}name:{{$name}} --}}
+        <div class="flex flex-col items-center">
+            <div class="w-full h-22">
               <x-layouts.header :id="$id" :name="$name"/>
+            </div>
+            <div class="mt-28 flex flex-col h-auto">
+              <div class="grid grid-cols-2 xl:grid-cols-4 gap-8 sm:gap-10 lg:gap-20 text-lg md:text-2xl lg:text-3xl">
+                @foreach($inHomeButton as $buttonName => $status)
+                  <x-layouts.button :buttonName="$buttonName" :status="$status" />
+                @endforeach
+              </div>
             </div>
         </div>
     </body>
-
 </body>
 </html>
