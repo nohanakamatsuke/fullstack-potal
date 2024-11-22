05bf36f0c59c1f21245ca205967ee2833698ae00
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-22 16:34:09 +0900

    メインのルーティングを定義

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\SessionController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+Route::get( '/', [ MainController::class, 'show_home' ] );
 
-Route::get('/', [SessionController::class, 'session']);
 //ログイン用ルーティング
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get( '/login', [ AuthController::class, 'showLoginForm' ] )->name( 'login' );
+Route::post( '/login', [ AuthController::class, 'login' ] )->name( 'login.attempt' );
+Route::post( '/logout', [ AuthController::class, 'logout' ] )->name( 'logout' );
