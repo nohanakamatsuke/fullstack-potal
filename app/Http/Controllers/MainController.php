04269d19258eaf9c5cd6d713a1de47e12fb5edb0
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-25 14:43:53 +0900

    セッションからヘッダー表示情報取得

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller {
 
-    public function show_home() {
+    public function show_home( Request $request ) {
         // ステータスを管理する配列
         $inHomeButton = [ '経費' => 1, '欠勤' . PHP_EOL . '遅刻' => 0, '有休' => 0, '勤務時間変更' . PHP_EOL . '休暇取得' =>0 ];
-        //ヘッダー出力情報固定値
-        $id = '_2222';
-        $name = '釜付 野花';
 
-        return view( 'home', compact( 'id', 'name', 'inHomeButton' ) );
+        $user_id = $request->session()->get( 'user_id' );
+        $name = $request->session()->get( 'name' );
+
+        return view( 'home', compact( 'user_id', 'name', 'inHomeButton' ) );
     }
 
 }
