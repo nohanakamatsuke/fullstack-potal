322c417795fc8aa4f6a664f676853a296dc31251
Y.inoue <kisin1010@gmail.com>
2024-11-26 14:31:39 +0900

    ログイン状態の際にログインにアクセスすると、homeへ遷移する実装完了

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController extends Controller {
-
-    public function show_home( Request $request ) {
+class MainController extends Controller
+{
+    public function show_home(Request $request)
+    {
         // ステータスを管理する配列
-        $inHomeButton = [ '経費' => 1, '欠勤' . PHP_EOL . '遅刻' => 0, '有休' => 0, '勤務時間変更' . PHP_EOL . '休暇取得' =>0 ];
+        $inHomeButton = ['経費' => 1, '欠勤'.PHP_EOL.'遅刻' => 0, '有休' => 0, '勤務時間変更'.PHP_EOL.'休暇取得' => 0];
 
-        $user_id = $request->session()->get( 'user_id' );
-        $name = $request->session()->get( 'name' );
+        $user_id = $request->session()->get('user_id'); //user_idをセッション情報から取得し、user_idに格納する
+        $name = $request->session()->get('name'); //nameをセッション情報から取得し、nameに格納する。
 
-        return view( 'home', compact( 'user_id', 'name', 'inHomeButton' ) );
+        return view('home', compact('user_id', 'name', 'inHomeButton')); //フロントのhome画面に、セッションから取得した値と、各種ボタンの表示を渡す。
     }
-
 }
