a9629640d17f999938890a93d2f3bf3f94b8d569
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-26 14:44:38 +0900

    ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ExpenseMenuController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'show_home'])->middleware('auth')->name('home'); //ログインしていない場合ログイン画面へ遷移する
+Route::get( '/', [ MainController::class, 'show_home' ] )->middleware( 'auth' )->name( 'home' );
+
+// 経費メニュー用ルーティング
+Route::get( '/expense-menu', [ ExpenseMenuController::class, 'show_expense_menu' ] )->name( 'expense' );
 
 //ログイン用ルーティング
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout.attempt');
+Route::get( '/login', [ AuthController::class, 'showLoginForm' ] )->name( 'login' );
+Route::post( '/login', [ AuthController::class, 'login' ] )->name( 'login.attempt' );
+Route::post( '/logout', [ AuthController::class, 'logout' ] )->name( 'logout.attempt' );
+
