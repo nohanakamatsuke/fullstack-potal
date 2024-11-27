960e8cfd3e1f1d2867bb332afe54607a62bb0cc8
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-27 18:29:31 +0900

    ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,14 @@ use Illuminate\Support\Facades\Route;
 Route::get( '/', [ MainController::class, 'show_home' ] )->middleware( 'auth' )->name( 'home' );
 
 // 経費メニュー用ルーティング
-Route::get( '/expense-menu', [ ExpenseMenuController::class, 'show_expense_menu' ] )->name( 'expense' );
+Route::get( '/expense-menu', [ ExpenseMenuController::class, 'show_expense_menu' ] )->middleware( 'auth' )->name( 'expense' );
 
 // 経費申請フォーム用ルーティング
-Route::get( '/expense-form', [ ExpenseFormController::class, 'show_expense_form' ] )->name( 'expense' );
+Route::get( '/expense-form', [ ExpenseFormController::class, 'show_expense_form' ] )->middleware( 'auth' )->name( 'expense' );
+Route::post( '/expense-form', [ ExpenseFormController::class, 'expense_form_submit' ] )->middleware( 'auth' )->name( 'expense' );
+
+// 確認画面用ルーティング
+Route::get( '/expense-confirm', [ ExpenseFormController::class, 'show_expense_confirm' ] )->middleware( 'auth' )->name( 'confirm' );
 
 //ログイン用ルーティング
 Route::get( '/login', [ AuthController::class, 'showLoginForm' ] )->name( 'login' );
