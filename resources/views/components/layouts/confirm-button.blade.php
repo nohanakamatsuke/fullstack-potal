fff4276e89338ddbd96e14e911829484540e2636
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-27 15:30:57 +0900

    ボタンをコンポーネント化

---
+++ resources/views/components/layouts/confirm-button.blade.php
@@ -0,0 +1,1 @@
+<button type="submit" class="mt-5 mb-14 w-full py-3 text-white border border-gray-800 bg-gray-700 rounded-lg">確認</button>
---
+++ resources/views/components/layouts/plus-button.blade.php
@@ -0,0 +1,3 @@
+<button type="button">
+  <x-heroicon-o-plus-circle class="w-10 h-10 mt-3" />
+</button>
