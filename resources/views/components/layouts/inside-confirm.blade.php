0ba9ee1e88afb4a73110b1017e16de5fee316687
nohanakamatsuke <nohana115ron@icloud.com>
2024-11-29 13:35:25 +0900

    確認画面作成

---
+++ resources/views/components/layouts/inside-confirm.blade.php
@@ -0,0 +1,24 @@
+<section id="form-confirm" class="bg-gray-300 mt-10 rounded-md ">
+  <div class="pl-8 pr-24 py-8">
+    <div class="flex border-b-2 border-gray-400">
+      <label>用途項目:</label>
+      <p>固定値</p>
+    </div>
+    <div class="flex border-b-2 border-gray-400">
+      <label>使用年月日:</label>
+      <p>2024/11/11</p>
+    </div>
+    <div class="flex border-b-2 border-gray-400">
+      <label>領収書の画像:</label>
+      <img src="" alt="">
+    </div>
+    <div class="flex border-b-2 border-gray-400">
+      <label>合計金額:</label>
+      <p>8888</p>
+    </div>
+    <div class="flex border-b-2 border-gray-400">
+      <label>用途の詳細:</label>
+      <p>テスト</p>
+    </div>
+  </div>
+</section>
