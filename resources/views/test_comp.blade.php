53f931ec866bedecf34a451af953889d1701d6c7
HonamiOta <h.oota0705@gmail.com>
2024-12-05 16:31:34 +0900

    test_comp.blatde.php追加

---
+++ resources/views/test_comp.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>申請完了</title>
+</head>
+{{-- テストようにバックで作成した申請完了ページ --}}
+
+<body>
+  <h1>申請が完了しました！</h1>
+  <a href="{{ route('expense.form') }}">戻る</a>
+</body>
+
+</html>
