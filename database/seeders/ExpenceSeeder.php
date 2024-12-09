2252570c9b842a3023a40024ea6d600209fec69c
Y.inoue <kisin1010@gmail.com>
2024-12-09 10:52:35 +0900

    csvダウンロード実装完了

--- database/seeders/ExpenceSeeder.php
+++ database/seeders/ExpenceSeeder.php
@@ -21,7 +21,33 @@ class ExpenceSeeder extends Seeder
             'item' => '交通費',
             'purpose' => '京橋から東京駅までの乗車賃',
             'total_amount' => '300',
-            'user_id' => '1', //userテーブルから取得予定
+            'user_id' => '_1234', //userテーブルから取得予定
+            'account_items' => '電車代',
+            'freee_sync_status' => '0',
+        ]);
+        ExpenseApp::create([
+            'expense_id' => '2',
+            'receipt_front' => 'test_photo1',
+            'receipt_back' => 'test_photo2',
+            'use_date' => '2024-12-03',
+            'expense_app_line_templates' => '交通費',
+            'item' => '交通費',
+            'purpose' => '京橋から東京駅までの乗車賃',
+            'total_amount' => '400',
+            'user_id' => '_5678', //userテーブルから取得予定
+            'account_items' => '電車代',
+            'freee_sync_status' => '0',
+        ]);
+        ExpenseApp::create([
+            'expense_id' => '3',
+            'receipt_front' => 'test_photo1',
+            'receipt_back' => 'test_photo2',
+            'use_date' => '2024-12-03',
+            'expense_app_line_templates' => '交通費',
+            'item' => '交通費',
+            'purpose' => '京橋から東京駅までの乗車賃',
+            'total_amount' => '500',
+            'user_id' => '_9012', //userテーブルから取得予定
             'account_items' => '電車代',
             'freee_sync_status' => '0',
         ]);
