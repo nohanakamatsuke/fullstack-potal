34736ddfc0982c1b20ed157fdef0cec32e946bab
nohanakamatsuke <nohana115ron@icloud.com>
2024-12-11 15:32:35 +0900

    CSV出力のボタン作成　権限情報取得

--- app/Http/Controllers/CsvTestExportController.php
+++ app/Http/Controllers/CsvTestExportController.php
@@ -5,17 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\ExpenseApp;
 use Illuminate\Support\Facades\Response;
 
-class CsvTestExportController extends Controller
-{
-    public function csvMonitor()
-    {
+class CsvTestExportController extends Controller {
+    public function csvMonitor() {
         $expenses = ExpenseApp::all();
 
-        return view('export-csv', compact('expenses'));
+        return view( 'export-csv', compact( 'expenses' ) );
     }
 
-    public function exportCsv()
-    {
+    public function exportCsv() {
+
         $expenses = ExpenseApp::all();
 
         $csvFileName = 'expenses.csv';
@@ -24,32 +22,35 @@ class CsvTestExportController extends Controller
             'Content-Disposition' => 'attachment; filename="'.$csvFileName.'"',
         ];
 
-        $callback = function () use ($expenses) {
-            $file = fopen('php://output', 'w');
+        $callback = function () use ( $expenses ) {
+            $file = fopen( 'php://output', 'w' );
 
             // ヘッダー行を追加
-            fputcsv($file, array_map(function ($header) {
-                return mb_convert_encoding($header, 'SJIS', 'UTF-8');
-            }, ['ユーザーID', '名前', '使用日', '経費項目', '金額', '目的', 'レシート（表）', 'レシート（裏）']));
+            fputcsv( $file, array_map( function ( $header ) {
+                return mb_convert_encoding( $header, 'SJIS', 'UTF-8' );
+            }
+            , [ 'ユーザーID', '名前', '使用日', '経費項目', '金額', '目的', 'レシート（表）', 'レシート（裏）' ] ) );
 
             // データをCSVに書き込む
-            foreach ($expenses as $expense) {
-                fputcsv($file, array_map(function ($field) {
-                    return mb_convert_encoding($field, 'SJIS', 'UTF-8');
-                }, [
+            foreach ( $expenses as $expense ) {
+                fputcsv( $file, array_map( function ( $field ) {
+                    return mb_convert_encoding( $field, 'SJIS', 'UTF-8' );
+                }
+                , [
                     $expense->user_id,
                     $expense->name,
                     $expense->use_date,
                     $expense->item,
                     $expense->total_amount,
                     $expense->purpose,
-                    $expense->receipt_front ? asset('storage/'.$expense->receipt_front) : '', // 画像のURLを生成
-                    $expense->receipt_back ? asset('storage/'.$expense->receipt_back) : '',  // 画像のURLを生成
-                ]));
+                    $expense->receipt_front ? asset( 'storage/'.$expense->receipt_front ) : '', // 画像のURLを生成
+                    $expense->receipt_back ? asset( 'storage/'.$expense->receipt_back ) : '',  // 画像のURLを生成
+                ] ) );
             }
-            fclose($file);
-        };
+            fclose( $file );
+        }
+        ;
 
-        return Response::stream($callback, 200, $headers);
+        return Response::stream( $callback, 200, $headers );
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,37 +1,56 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class MainController extends Controller
-{
+class MainController extends Controller {
     // クラスレベルで、protectedで変数を定義することで、子クラスからアクセス可能
     protected $user_id;
 
     protected $name;
 
-    public function show_home(Request $request)
-    {
+    public function show_home( Request $request ) {
         //protectedにアクセスするために$thisを通す
         $this->user_id
-        = $request->session()->get('user_id');
+        = $request->session()->get( 'user_id' );
         $this->name
-        = $request->session()->get('name');
-        // ホームボタンの情報を、label, status, routeの配列で指定する
-        $inHomeButton = [
-            ['label' => '経費', 'status' => 1, 'route' => 'expense-menu'],
-            ['label' => '欠勤'.PHP_EOL.'遅刻', 'status' => 0, 'route' => ''],
-            ['label' => '有休', 'status' => 0, 'route' => ''],
-            ['label' => '勤務時間変更'.PHP_EOL.'休暇取得', 'status' => 0, 'route' => ''],
-        ];
+        = $request->session()->get( 'name' );
+
         // compactは通常の変数しか扱えないので、$thisのプロパティをローカル変数に変換
         $name = $this->name;
         $user_id = $this->user_id;
+
+        // Userテーブルからセッション情報と一致するユーザー情報を取得する
+        $user = User::find( $user_id )->getAttributes();
+
+        // role_idキーのみ取得
+        $role = $user[ 'role_id' ];
+
+        // ホームボタンの情報を、label, status, routeの配列で指定する
+        $homeButton = [
+            [ 'label' => '経費', 'status' => 1, 'route' => 'expense-menu', 'role' => 1 ],
+            [ 'label' => '欠勤'.PHP_EOL.'遅刻', 'status' => 0, 'route' => '', 'role' => 1 ],
+            [ 'label' => '有休', 'status' => 0, 'route' => '', 'role' => 1 ],
+            [ 'label' => '勤務時間変更'.PHP_EOL.'休暇取得', 'status' => 0, 'route' => '', 'role' => 1 ],
+            // [ 'label' => 'CSV', 'status' => 1, 'route' => 'csvMonitor', 'role' => 2 ],
+        ];
+
+        // ユーザーの権限によって、ビューに渡す配列をフィルタリングする
+        $fillterdHomeButton = collect($homeButton)->filter(function($button) use($role){
+
+          // 管理者の場合は、全てのボタンを表示
+          if($role === 2){
+            return true;
+          };
+          // それ以外は、role = 1のボタンのみ表示
+          return $button['role'] === 1;
+        })->all();
+
         //直前のページURLを取得
         $prevurl = url()->previous();
 
-        return view('home', compact('user_id', 'name', 'inHomeButton'));
+        return view( 'home', compact( 'user_id', 'name', 'fillterdHomeButton', 'role' ) );
         //フロントのhome画面に、セッションから取得した値と、各種ボタンの表示を渡す。
     }
 }
