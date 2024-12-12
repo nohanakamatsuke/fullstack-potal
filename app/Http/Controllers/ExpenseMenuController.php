931ce7b9bdd218a052228d1f4a37fbb9d47be217
nohanakamatsuke <nohana115ron@icloud.com>
2024-12-12 11:12:27 +0900

    レイアウト修正

--- app/Http/Controllers/ExpenseMenuController.php
+++ app/Http/Controllers/ExpenseMenuController.php
@@ -7,52 +7,47 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ExpenseApp;
 
-class ExpenseMenuController extends MainController
-{
-    public function show_expense_menu(Request $request)
-    {
+class ExpenseMenuController extends MainController {
+    public function show_expense_menu( Request $request ) {
         // 親クラスから、メソッドを呼び出す
-        $this->show_home($request);
+        $this->show_home( $request );
 
         // compactは通常の変数しか扱えないので、$thisのプロパティをローカル変数に変換
         $name = $this->name;
         $user_id = $this->user_id;
 
         $inExpenseMenuButton = [
-            ['label' => '申請', 'status' => 1, 'route' => 'expense-form'],
-            ['label' => '履歴', 'status' => 0, 'route' => ''],//後で消す　ここに履歴一覧のページを入れれば飛ばせる
+            [ 'label' => '申請', 'status' => 1, 'route' => 'expense-form' ],
+            [ 'label' => '履歴', 'status' => 0, 'route' => '' ], //後で消す　ここに履歴一覧のページを入れれば飛ばせる
         ];
         // ExpenseAppモデルから必要なカラムのみ取得
-        $expenses = ExpenseApp::where('user_id', $user_id)
-            ->select('use_date', 'item', 'total_amount', 'freee_sync_status')
-            ->orderBy('use_date', 'desc')
-            ->get();
+        $expenses = ExpenseApp::where( 'user_id', $user_id )
+        ->select( 'use_date', 'item', 'total_amount', 'freee_sync_status' )
+        ->orderBy( 'use_date', 'desc' )
+        ->get();
 
         $expenseHistory = [];
-        foreach ($expenses->take(6) as $expense) {
+        foreach ( $expenses->take( 6 ) as $expense ) {
             // freee_sync_statusに応じて承認状態を設定
-            $expense_status = ($expense->freee_sync_status === 0) ? '承認' : '未承認';
+            $expense_status = ( $expense->freee_sync_status === 0 ) ? '承認' : '未承認';
 
             // 日付、項目、金額を整形して履歴に追加
-            $key = date('Y/m/d', strtotime($expense->use_date)) . ' ' .
-                $expense->item . ' ￥' . number_format($expense->total_amount);
-            $expenseHistory[$key] = $expense_status;
+            $key = date( 'Y/m/d', strtotime( $expense->use_date ) ) . ' ' .
+            $expense->item . PHP_EOL.' ￥' . number_format( $expense->total_amount );
+            $expenseHistory[ $key ] = $expense_status;
         }
-
 
         //直前のページURLを取得
         $prevurl = url()->previous();
 
-        return view('expense-menu', compact('user_id', 'name', 'inExpenseMenuButton', 'expenseHistory', 'prevurl','expenses'));
+        return view( 'expense-menu', compact( 'user_id', 'name', 'inExpenseMenuButton', 'expenseHistory', 'prevurl', 'expenses' ) );
     }
 
-    public function create()
-    {
+    public function create() {
         // 経費申請フォーム
     }
 
-    public function history()
-    {
+    public function history() {
         // 経費一覧
     }
 }
